da78278a8e7c8641931353d6c663154d995c76f8
Joviano Dias <jovianodias@Jovianos-MacBook-Pro.local>
2016-03-11 15:30:42 +0000

    First commit

--- features/feature_files/rejects.feature
+++ features/feature_files/rejects.feature
@@ -1,22 +1,33 @@
 Feature: Track manuscripts that have been rejected by editorial department
 
-As a member of the editorial department
-In order to see how many manuscripts have been rejected for all relevant journals
-I need to be able to see a display of all manuscripts that have been rejected by us and if applicable accepted and published by other organisations
+  As a member of the editorial department
+  In order to see how many manuscripts have been rejected for all relevant journals
+  I need to be able to see a display of all manuscripts that have been rejected by us and if applicable accepted and published by other organisations
 
+#
+#  Scenario Outline: I am able to see a summary of rejection statistics by publisher and journal
+#
+#    Given I am a <role>
+#    And I have selected to view <brand> rejections
+#    When I click on the first destination journal title
+#    Then I see the <expected> page
+#
+#    Examples:
+#      | role      | brand    | expected            |
+#      | user      | bmc      | no access           |
+#      | superuser | bmc      | rejection breakdown |
+#      | user      | nature   | no access           |
+#      | superuser | nature   | rejection breakdown |
+#      | user      | springer | no access           |
+#      | superuser | springer | rejection breakdown |
 
-Scenario Outline: I am able to see a summary of rejection statistics by publisher and journal
+  Scenario Outline: I am able to see the journal destination page correctly
 
-Given I am a <role>
-And I have selected to view <brand> rejections
-When I click on the first destination journal title
-Then I see the <expected> page
+    Given I am on the destination journal page in the rejection breakdown for the <brand> brand
+    Then the destination journal page is the same as the one accessed from the rejection breakdown page
 
-Examples:
-  |role|brand|expected|
-  |user|bmc|no access|
-  |superuser|bmc|rejection breakdown|
-  |user|nature|no access|
-  |superuser|nature|rejection breakdown|
-  |user|springer|no access|
-  |superuser|springer|rejection breakdown|
+    Examples:
+      | brand    |
+      | bmc      |
+      | nature   |
+      | springer |
--- features/step_definitions/results/page_steps.rb
+++ features/step_definitions/results/page_steps.rb
@@ -1,38 +1,53 @@
 Then (/^I see the (.*) page$/) do |result|
 
   case result
-  when 'no access'
-    title = @app.no_access.title.to_s
-    expect(title).to eq("Springer Nature")
-    expect(@app.no_access).to have_message
-    # Value checks
-    current_page = @app.no_access.title
-    puts "No Access: " + current_page
-  when 'rejection breakdown'
-    title = @app.rejection_list.title.to_s
-    if title =~ /All BioMed Central/i
-      expect(title).to eq("Springer Nature Rejection Tracker for BioMed Central : All BioMed Central journals")
-      expect(@app.rejection_list).to have_rejected_from_heading
-      expect(@app.rejection_list).to have_title_heading
-      expect(@app.rejection_list).to have_rejected_heading
-      expect(@app.rejection_list).to have_authors_heading
-      expect(@app.rejection_list).to have_published_title_heading
-    elsif title =~ /All Nature/i
-      expect(title).to eq("Springer Nature Rejection Tracker for Nature : All Nature journals")
-      expect(@app.rejection_list).to have_rejected_from_heading
-      expect(@app.rejection_list).to have_title_heading
-      expect(@app.rejection_list).to have_rejected_heading
-      expect(@app.rejection_list).to have_authors_heading
-      expect(@app.rejection_list).to have_published_title_heading
-    elsif title =~ /All Springer/i
-      expect(title).to eq("Springer Nature Rejection Tracker for Springer : All Springer journals")
-      expect(@app.rejection_list).to have_rejected_from_heading
-      expect(@app.rejection_list).to have_title_heading
-      expect(@app.rejection_list).to have_rejected_heading
-      expect(@app.rejection_list).to have_authors_heading
-      expect(@app.rejection_list).to have_published_title_heading
-    end
-    current_page = @app.rejection_list.title
-    puts "Rejection Breakdown: " + current_page
+    when 'no access'
+      title = @app.no_access.title.to_s
+      expect(title).to eq("Springer Nature")
+      expect(@app.no_access).to have_message
+      # Value checks
+      current_page = @app.no_access.title
+      puts "No Access: " + current_page
+    when 'rejection breakdown'
+      title = @app.rejection_list.title.to_s
+      if title =~ /All BioMed Central/i
+        expect(title).to eq("Springer Nature Rejection Tracker for BioMed Central : All BioMed Central journals")
+        expect(@app.rejection_list).to have_rejected_from_heading
+        expect(@app.rejection_list).to have_title_heading
+        expect(@app.rejection_list).to have_rejected_heading
+        expect(@app.rejection_list).to have_authors_heading
+        expect(@app.rejection_list).to have_published_title_heading
+      elsif title =~ /All Nature/i
+        expect(title).to eq("Springer Nature Rejection Tracker for Nature : All Nature journals")
+        expect(@app.rejection_list).to have_rejected_from_heading
+        expect(@app.rejection_list).to have_title_heading
+        expect(@app.rejection_list).to have_rejected_heading
+        expect(@app.rejection_list).to have_authors_heading
+        expect(@app.rejection_list).to have_published_title_heading
+      elsif title =~ /All Springer/i
+        expect(title).to eq("Springer Nature Rejection Tracker for Springer : All Springer journals")
+        expect(@app.rejection_list).to have_rejected_from_heading
+        expect(@app.rejection_list).to have_title_heading
+        expect(@app.rejection_list).to have_rejected_heading
+        expect(@app.rejection_list).to have_authors_heading
+        expect(@app.rejection_list).to have_published_title_heading
+      end
+      current_page = @app.rejection_list.title
+      puts "Rejection Breakdown: " + current_page
   end
+end
+
+
+Given (/^I am on the destination journal page in the rejection breakdown for the (bmc|springer|nature) brand$/) do |brand|
+
+  step "I am a superuser"
+  step "I have selected to view #{brand} rejections"
+  @first_link_text=@app.tracker.rejection_chart_first_link.text.split(" : ").last
+  step "I click on the first destination journal title"
+end
+
+Then (/^the destination journal page is the same as the one accessed from the rejection breakdown page$/) do
+
+  puts @app.rejection_list.rejection_list_title.text
+  expect(@app.rejection_list.rejection_list_title.text).to include(@first_link_text)
 end
