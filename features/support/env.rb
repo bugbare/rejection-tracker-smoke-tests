5bc15b0582ce4d02dde5740c3b41faab5fd6ac50
Joviano Dias <jovianod10@gmail.com>
2016-03-18 16:33:44 +0000

    [Jov] Moved hard-coded data to yml

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,9 +1,10 @@
 require 'capybara'
 
-
   case ENV['TARGET']
    when 'staging' then Capybara.app_host = 'http://rejection-dev.dev.cf.private.springer.com/'
     puts  "\n>>>> ENVIRONMENT = STAGING <<<<\n\n"
    when 'live' then Capybara.app_host = 'http://rejection-dev.dev.cf.private.springer.com/'
     puts  "\n>>>> ENVIRONMENT = LIVE <<<<\n\n"
   end
+
+  $product_data=YAML.load_file(File.dirname(__FILE__) + '/../../resources/test-data/' + "product_data.yml")
